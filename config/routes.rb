76a478fc8e47473ccecdde40b1c4ef79e22fb388
appleRui <apple31581@gmail.com>
2021-08-24 20:14:07 +0900

    静的ページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  get 'static_pages/help'
+  root 'foo#hello'
   resources :tweets
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'hello-world' => "foo#hello"
+  get 'home' => "static_pages#home"
+  get 'help' => "static_pages#help"
+  get 'about' => "static_pages#about"
+  get 'contact' => "static_pages#contact"
 end

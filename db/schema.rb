1e726585d6050b824e65b2d222bc87c92f007a9f
appleRui <apple31581@gmail.com>
2021-08-24 21:03:21 +0900

    validation and password

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_10_103049) do
+ActiveRecord::Schema.define(version: 2021_08_24_114846) do
 
   create_table "tweets", force: :cascade do |t|
     t.text "content"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2021_08_10_103049) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
